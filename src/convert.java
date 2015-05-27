5f52e785c35da2ea5973d35df7fcdc84908c9c4b
maheshbr <maheshb@sahajsoft.com>
2015-05-27 09:33:08 +0530

    Initial commit

---
+++ src/convert.java
@@ -0,0 +1,22 @@
+/**
+ * Created by maheshbr on 14/04/15.
+ */
+public class convert {
+    private double round(double val)
+    {
+        val = val * 100;
+        val = Round
+    }
+    public double convertInchesToCentimeter(double inches)
+    {
+        return ( (2.54 * inches) ;
+    }
+
+    public double convertInchesToMM(double inches){
+        return 25.4 * inches;
+    }
+
+    public double convertInchesToMeter(double inches){
+        return .0254 * inches;
+    }
+}
